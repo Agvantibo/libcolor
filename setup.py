07404db46906ec24b5cfa06e2bf0f3029f2740f8
Agvantibo <68030524+Agvantibo@users.noreply.github.com>
2020-12-02 12:26:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="libcolor-agvantibo", # Replace with your own username
+    name="libcolor", # Replace with your own username
     version="0.0.1",
     author="Savchenko Dmitriy",
     author_email="d.savchenko@sch-int.ru",
     description="A light toolkit to manage Tk colors",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Agvantibo//libcolor",
+    url="https://github.com/Agvantibo/libcolor",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
